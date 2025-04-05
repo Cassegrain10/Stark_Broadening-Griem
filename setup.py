d47c1258652e4c9af73a9c1a1c77d6dfe56abb48
Cassegrain10 <128841423+Cassegrain10@users.noreply.github.com>
2025-04-04 20:33:10 -0400

    Create setup.py
    Adding setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="griem",
+    version="0.1.0",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "scipy",
+        "tabulate"
+    ],
+    author="Jordan Mindrup",
+    description="Stark broadening calculation package using Griem theory",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Cassegrain10/Stark_Broadening-Griem",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.7',
+)
