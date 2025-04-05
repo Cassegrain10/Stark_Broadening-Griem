956cf9e337f31fddf1b7ce347cc2a9e486405911
Cassegrain10 <128841423+Cassegrain10@users.noreply.github.com>
2025-04-04 20:51:01 -0400

    Update constants.py

--- griem/constants.py
+++ griem/constants.py
@@ -14,7 +14,7 @@ ANGULAR_MOMENTUM_QUANTUM_NUMBERS = {
 }
 
 # Physical constants
-SPEED_OF_LIGHT = const.c        # [m/s]
-H_BAR = const.hbar              # [J s]
-ELECTRON_MASS = const.me        # [kg]
-BOLTZMANN_CONSTANT = const.k    # [J/K]
+SPEED_OF_LIGHT = 2.99792458e+08        # [m/s]          Speed of light
+H_BAR = 1.054571817e-34                # [J s]          Planck's
+ELECTRON_MASS = 9.1093837015e-31       # [kg]           Mass of electron
+BOLTZMANN_CONSTANT = 1.380649e-23      # [J/K]          Boltzmann's constant
