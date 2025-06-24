8d3074f5e2978138eff3baba290411b2004991c5
Jordan Mindrup <128841423+Cassegrain10@users.noreply.github.com>
2025-06-23 21:03:30 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setup(
     ],
     author="Jordan Mindrup",
     description="Stark broadening calculation package using Griem theory",
-    long_description=open("README.md").read(),
+    long_description = open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url="https://github.com/Cassegrain10/Stark_Broadening-Griem",
     classifiers=[
