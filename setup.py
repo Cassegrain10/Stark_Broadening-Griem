4f7c00f47ba3d06ffdbe6800d7cbcc4bece1c37b
Jordan Mindrup <128841423+Cassegrain10@users.noreply.github.com>
2025-07-13 00:18:49 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,19 @@
 from setuptools import setup, find_packages
+from pathlib import Path
+
+# Load dependencies from requirements.txt
+requirements_path = Path(__file__).parent / "requirements.txt"
+requirements = requirements_path.read_text(encoding="utf-8").splitlines()
 
 setup(
     name="griem",
     version="0.1.0",
     packages=find_packages(),
     include_package_data=True,
-    install_requires=[
-        "numpy",
-        "pandas",
-        "scipy",
-        "tabulate"
-    ],
+    install_requires=requirements,
     author="Jordan Mindrup",
     description="Stark broadening calculation package using Griem theory",
-    long_description = open("README.md", encoding="utf-8").read(),
+    long_description=Path("README.md").read_text(encoding="utf-8"),
     long_description_content_type="text/markdown",
     url="https://github.com/Cassegrain10/Stark_Broadening-Griem",
     classifiers=[
